2a57c0cab647aad03355713e40ed071875318a8c
Kavinda Rajapakse <kavindasr@gmail.com>
2025-05-01 12:36:34 +0530

    Add albCertArn parameter to configure-cluster script for enhanced flexibility

--- utils/configure-cluster.groovy
+++ utils/configure-cluster.groovy
@@ -31,6 +31,7 @@ String tfS3region = params.tfS3region
 String awsCred = params.awsCred
 String dbPassword = params.dbPassword
 String project = params.project?: "wso2"
+String albCertArn = params.albCertArn ?: ""
 
 // Default values
 def deploymentPatterns = []
